f845e730bf3497cc4c894267740ecb413128e880
Andrew Soozay <asousa2021@gmail.com>
2024-07-13 13:16:40 -0700

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,5 +1,64 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+// Andrew Sousa
+// 7/12/2024
+// CS 145
+// Programming Assignment 3: Employee Hierarchy
+// Employee.java
+
+// Employee.java is a superclass for employee types in the program. Every exmployee has attirbutes of first name, last name, and social security number.
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+public class Employee {
+    protected String firstName;
+    protected String lastName;
+    protected String socialSecurityNumber;
+
+    // method: Employee (no return type)
+    // purpose: constructs the attributes for the object
+    // parameters:  (1) firstName (String):  the employee's first name
+    //              (2) lastName (String): the employee's last name
+    //              (3) socialSecurityNumber (String): the employee's social security number
+    // NOTES: SSn must be entered as XX-XXX-XXXX, or an IllegalArgumentException will be thrown
+    protected Employee(String firstName, String lastName, String socialSecurityNumber){
+        if(!(socialSecurityNumber.charAt(3)+ "").equals("-") || !(socialSecurityNumber.charAt(6) + "").equals("-")){
+            throw new IllegalArgumentException("Social Security Number must be in format XXX-XX-XXXX");
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
     }
+
+
+    // method: getFirstName (String)
+    // purpose: returns the employee's first name
+    // parameters: none
+    protected String getFirstName(){
+        return firstName;
+    }
+
+
+    // method: getFirstName (String)
+    // purpose: returns the employee's first name
+    // parameters: none
+    protected String getLastName(){
+        return lastName;
+    }
+
+
+    // method: getSocialSecurityNumber(String)
+    // purpose: returns the employee's social security number
+    // parameters: none
+    protected String getSocialSecurityNumber(){
+        return socialSecurityNumber;
+    }
+
+
+    // method: toString (String)
+    // purpose: returns the employee information as a single string
+    // parameters: none
+    public String toString(){
+        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f",
+                "Employee ", getFirstName(), getLastName(),
+                "social security number", getSocialSecurityNumber());
+    }
+
 }
